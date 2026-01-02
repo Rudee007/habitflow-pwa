b35f7fa83fdf1579d43d68b86c31c5f4463dce92
Rudee007 <nikuswain01171432@gmail.com>
2026-01-02 14:05:19 +0530

    installed hyperspeed component

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-51749ddc'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kupotn7thtg"
+    "revision": "0.41j21e0e2p8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
